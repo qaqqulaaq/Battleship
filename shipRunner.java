6f7a3d6bb1f541579a9085215e479f4ff6743fca
PlantsKing2 <isaiajudkins@gmail.com>
2026-03-30 11:44:12 -0800

    Update shipRunner.java

--- shipRunner.java
+++ shipRunner.java
@@ -1,5 +1,5 @@
 
-public class shipRunner { public static void main(String[]args){
+public class help { public static void main(String[]args){
         String adrian = "hello";
         String aticus = "xddcc";
       String[][] atigun = {{"_","1","2","3","4","5","6","7","8","9","10"},
