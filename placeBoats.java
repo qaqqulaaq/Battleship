a4d5e6a2e9f5e0c9b5884e83895a3a2809ac4d4c
BornOnThursday <ultraviolet.thomas@gmail.com>
2026-04-07 11:17:18 -0800

    placeBoats VIOLET
    everything is working and i have the prompt for when the spot is already filled, but it doesnt let you reenter for the given ship yet.

--- placeBoats.java
+++ placeBoats.java
@@ -28,6 +28,8 @@ int[] allBoatsUnits = {5,4,3,3,2};
 String[] allBoatsNames = {"Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer"};
 
 
+
+
 public void userPlaces(){
     for(int i = 0; i < allBoatsUnits.length; i++){
     
@@ -42,12 +44,7 @@ public void userPlaces(){
     System.out.println("Tell me the the y value you want the bow of your boat to be on");
     int bowY = input.nextInt();
     input.nextLine();
-    // System.out.println("Tell me the x value you want the stern of your boat to be on (remember the boat has to extend " + thisBoatsUnits + " spaces after the bow)");
-    // int sternX = input.nextInt();
-    // input.nextLine();
-    // System.out.println("Tell me the y value you want the stern of your boat to be on (remember the boat has to extend " + thisBoatsUnits + " spaces after the bow)");
-    // int sternY = input.nextInt();
-    // input.nextLine();   
+   
    
 
     System.out.println("Now tell me the direction you want your boat to extend in (down, left, right)");
@@ -55,26 +52,47 @@ public void userPlaces(){
 
     if(direction.equals("down")){
        if(thisBoatsUnits == 2){
+        if(allSpots[bowX][bowY] == 1){
+          System.out.println("This spot is already filled; try again");
+        }
+        else{
         allSpots[bowX][bowY] = 1;
         allSpots[bowX+1][bowY] = 1;
+        }
        }
        else if(thisBoatsUnits == 3){
+        if(allSpots[bowX][bowY] == 1){
+          System.out.println("This spot is already filled; try again");
+        }
+        else {
         allSpots[bowX][bowY] = 1;
         allSpots[bowX+1][bowY] = 1;
         allSpots[bowX+2][bowY] = 1;
+        }
        }
        else if(thisBoatsUnits == 4){
+        if(allSpots[bowX][bowY] == 1){
+          System.out.println("This spot is already filled; try again");
+        }
+        else {
         allSpots[bowX][bowY] = 1;
         allSpots[bowX+1][bowY] = 1;
         allSpots[bowX+2][bowY] = 1;
         allSpots[bowX+3][bowY] = 1;
+        }
        }
        else if(thisBoatsUnits == 5){
+        if(allSpots[bowX][bowY] == 1){
+          System.out.println("This spot is already filled; try again");
+        
+        }
+        else {
         allSpots[bowX][bowY] = 1;
         allSpots[bowX+1][bowY] = 1;
         allSpots[bowX+2][bowY] = 1;
         allSpots[bowX+3][bowY] = 1;
         allSpots[bowX+4][bowY] = 1;
+        }
        }
     }
       else if(direction.equals("right")){
