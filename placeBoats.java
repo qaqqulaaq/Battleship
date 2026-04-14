ca5e3692dc1536ac5656aaff0e50b0b3b0100de8
BornOnThursday <ultraviolet.thomas@gmail.com>
2026-04-14 11:37:47 -0800

    placeboats VIOLET
    yeah

--- placeBoats.java
+++ placeBoats.java
@@ -20,13 +20,7 @@ static int[][] allSpots= {{0,0,0,0,0,0,0,0,0,0},
 int thisBoatsUnits = 0;
 String placingPrompt = "";
 
-// public void placingPrompt(int thisBoatsUnits){
-//     thisBoatsUnits = this.thisBoatsUnits;
-//     String thisBoatsUnit = null;
-//     System.out.println("The current boat is" + thisBoatsUnit + "units wide. On what coordinates would you like the bow (it will extend" + thisBoatsUnits + "spaces after that)?");
-// // // this is the prompt that tells the user what's going on
 
-//   }
 
 int[] allBoatsUnits = {5,4,3,3,2};
 String[] allBoatsNames = {"Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer"};
@@ -54,6 +48,8 @@ public void userPlaces(){
     System.out.println("Tell me the the y value you want the bow of your boat to be on");
     int bowY = input.nextInt();
     input.nextLine();
+
+    //these are the user prompts again
    
    
 
@@ -61,9 +57,13 @@ public void userPlaces(){
     String direction = input.nextLine();
 
     if(direction.equals("down")){
+      //if the next line was down, then the boats goes downwards
+      //only if the other spots aren't already filled, but that part isnt working yet
        if(thisBoatsUnits == 2){
         if(allSpots[bowX][bowY] == 1 || allSpots[bowX+1][bowY] == 1){
           System.out.println("This spot is already filled; womp womp restart the entire program");
+          i = 0;
+          //if the spots are already filled, the you have to restart
         }
         else{
         allSpots[bowX][bowY] = 1;
@@ -109,6 +109,7 @@ public void userPlaces(){
         if(thisBoatsUnits == 2){
             if(allSpots[bowX][bowY] == 1 || allSpots[bowX+1][bowY] == 1){
               System.out.println("This spot is already filled; womp womp restart the entire program");
+              //this is the same thing but the x changed because you're going to the right now
             }
             else{
             allSpots[bowX][bowY] = 1;
@@ -196,14 +197,15 @@ public void userPlaces(){
    
 
 
-    System.out.println(java.util.Arrays.toString(placeBoats.allSpots[0]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[1]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[2]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[3]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[4]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[5]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[6]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[7]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[8]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[9]));
-
+   
+//this print statement prints the grid row by row with the line breaks so that it actually looks like a grid. 
 
     
   }
 
 
     }
+     System.out.println(java.util.Arrays.toString(placeBoats.allSpots[0]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[1]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[2]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[3]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[4]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[5]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[6]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[7]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[8]) + "\n" + java.util.Arrays.toString(placeBoats.allSpots[9]));
 }
 
 }
